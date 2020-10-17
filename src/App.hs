5176d2e756e130f0c06c149e788dac77329a036b
Bertalan Kis <kbertalan@gmail.com>
2020-10-17 23:08:56 +0200

    hasql th

--- src/App.hs
+++ src/App.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes #-}
 module App
   ( app
   ) where
@@ -7,14 +7,11 @@ import Data.Int
 import Control.Exception (bracket)
 import Control.Monad.IO.Class
 import Options.Applicative
-import Hasql.Connection as DB
-import Hasql.Statement as DB
 import Hasql.Session as DB
-import Hasql.Encoders as E
-import Hasql.Decoders as D
 import Hasql.Pool as Pool
 import Hasql.OptparseApplicative as DB
-import Web.Scotty
+import Hasql.TH as TH
+import Web.Scotty as S
 
 data AppOptions = AppOptions
   { port :: !Int
@@ -42,12 +39,14 @@ app = do
   opts <- execParser appInfo
   bracket (Pool.acquire $ db opts) Pool.release $ \pool ->
     scotty (port opts) $ do
-      get "/hello" $ Web.Scotty.json ["hello"::String]
+      get "/hello" $ S.json ["hello"::String]
       get "/bello" $ do
         lit <- liftIO $ use pool selectLiteral
         case lit of
-          Left e -> liftIO (print e) >> Web.Scotty.json ("Error"::String)
-          Right l -> Web.Scotty.json l
+          Left e -> liftIO (print e) >> S.json ("Error"::String)
+          Right l -> S.json l
 
 selectLiteral :: DB.Session Int64
-selectLiteral = DB.statement () $ Statement "select 1" E.noParams (D.singleRow $ D.column $ D.nonNullable $ D.int8) True
+selectLiteral = DB.statement () $ [TH.singletonStatement|
+  select 1 :: int8
+  |]
