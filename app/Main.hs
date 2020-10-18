7e0fc7b3f1e04b5c650e4f6da1a25cd3f0c147a5
Bertalan Kis <kbertalan@gmail.com>
2020-10-18 14:49:56 +0200

    move options parsing to app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import App
+import Options.Applicative as O
+import Hasql.OptparseApplicative
 
 main :: IO ()
-main = app
+main = execParser appInfo >>= app
+
+options :: O.Parser Options
+options = Options
+  <$> option auto
+    ( long "port"
+    <> help "Web server port"
+    <> value 3000
+    <> showDefault
+    <> metavar "INT"
+    )
+  <*> poolSettings ("db-" <>)
+
+appInfo = info (options <**> helper)
+  $ fullDesc
+  <> progDesc "Simple TODO web app"
+
--- src/App.hs
+++ src/App.hs
@@ -1,49 +1,29 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
 module App
   ( app
+  , App.Options(..)
   ) where
 
 import Data.Aeson.Types as A
-import Data.Int
 import Data.Text.Lazy as T
 import Control.Exception (bracket)
 import Control.Monad.IO.Class
-import Options.Applicative as O
 import GHC.Generics (Generic)
-import Hasql.Session as DB
 import Hasql.Pool as Pool
-import Hasql.OptparseApplicative as DB
-import Hasql.TH as TH
 import Network.HTTP.Types.Status
 import Web.Scotty as S
 
 import Health (healthApi)
 import Todo (todoApi)
 
-data AppOptions = AppOptions
+data Options = Options
   { port :: !Int
   , db :: Pool.Settings
   }
   deriving (Show)
 
-appOptions :: O.Parser AppOptions
-appOptions = AppOptions
-  <$> option auto
-    ( long "port"
-    <> help "Web server port"
-    <> value 3000
-    <> showDefault
-    <> metavar "INT"
-    )
-  <*> DB.poolSettings ("db-" <>)
-
-appInfo = info (appOptions <**> helper)
-  $ fullDesc
-  <> progDesc "Simple TODO web app"
-
-app :: IO ()
-app = do
-  opts <- execParser appInfo
+app :: App.Options -> IO ()
+app opts = do
   bracket (Pool.acquire $ db opts) Pool.release $ \pool ->
     scotty (port opts) $ do
       defaultHandler $ \message -> do
