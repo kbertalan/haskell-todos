4accae2001af5645f7bd40de0a1715f5fc5c9dbb
Bertalan Kis <kbertalan@gmail.com>
2020-10-24 15:59:27 +0200

    use custom parameters

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import App
+import Data.String (fromString)
 import Options.Applicative as O
-import Hasql.OptparseApplicative
+import Hasql.Connection as Connection
+import Hasql.Pool as Pool
 
 main :: IO ()
 main = execParser appInfo >>= app
@@ -16,7 +18,60 @@ options = Options
     <> showDefault
     <> metavar "INT"
     )
-  <*> poolSettings ("db-" <>)
+  <*> poolSettings
+
+poolSettings :: Parser Pool.Settings
+poolSettings =
+  (,,) <$> size <*> timeout <*> connectionSettings
+  where
+    size =
+      option auto $
+        long ("pool-size") <>
+        value 2 <>
+        showDefault <>
+        help "Amount of connections in the pool"
+    timeout =
+      fmap fromIntegral $
+      option auto $
+        long ("pool-timeout") <>
+        value (10 :: Integer) <>
+        showDefault <>
+        help "Amount of seconds for which the unused connections are kept open"
+
+connectionSettings :: Parser Connection.Settings
+connectionSettings =
+  Connection.settings <$> host <*> port <*> user <*> password <*> name
+  where
+    host =
+      fmap fromString $ strOption $
+        long ("db-host") <> 
+        value "127.0.0.1" <>
+        showDefault <>
+        help "Server host"
+    port =
+      option auto $
+        long ("db-port") <>
+        value 5432 <>
+        showDefault <>
+        help "Server port"
+    user =
+      fmap fromString $ strOption $
+        long ("db-user") <>
+        value "postgres" <>
+        showDefault <>
+        help "Username"
+    password =
+      fmap fromString $ strOption $
+        long ("db-password") <>
+        value "postgres" <>
+        showDefault <>
+        help "Password"
+    name =
+      fmap fromString $ strOption $
+        long ("db-name") <>
+        value "postgres" <>
+        showDefault <>
+        help "Database name"
 
 appInfo :: ParserInfo Options
 appInfo = info (options <**> helper)
