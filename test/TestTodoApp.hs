1757e14677e2d5ba16997be67bbe680794bc053b
Bertalan Kis <kbertalan@gmail.com>
2020-11-30 21:48:02 +0100

    refactor test monad

--- test/TestTodoApp.hs
+++ test/TestTodoApp.hs
@@ -8,39 +8,26 @@ import           Control.Monad.State.Strict  (MonadState, State, runState)
 import qualified Control.Monad.State.Strict  as State
 import           Control.Monad.Trans         (lift)
 import           Data.Foldable               (find)
-import           Data.UUID                   (UUID)
 import           Todo.Domain
 
-newtype TestApp a = TestApp
+newtype TestTodoM a = TestTodoM
   { runApp :: (RandT StdGen) (State [Todo]) a
   } deriving newtype (Functor, Applicative, Monad, MonadRandom, MonadState [Todo])
 
-instance Logic TestApp where
-  showAll :: TestApp [Todo]
-  showAll = undefined
-
-  create :: CreateTodoRequest -> TestApp Todo
+instance Logic TestTodoM where
+  showAll = repoSelectAll
   create = logicCreate
-
-  modify :: UUID -> Todo -> TestApp (Either ModifyError Todo)
   modify = fmap runExceptT . logicUpdate
-
-  patch :: UUID -> TodoMaybe -> TestApp (Either PatchError Todo)
   patch = fmap runExceptT . logicPatch
-
-  delete :: UUID -> TestApp (Either DeleteError ())
   delete = fmap runExceptT logicDelete
 
-instance Repo TestApp where
-  repoSelectAll :: TestApp [Todo]
+instance Repo TestTodoM where
   repoSelectAll = State.get
 
-  repoInsert :: Todo -> TestApp Todo
   repoInsert todo = do
     State.modify (todo:)
     return todo
 
-  repoUpdate :: Todo -> TestApp Todo
   repoUpdate todo = do
     State.modify replace
     return todo
@@ -48,27 +35,25 @@ instance Repo TestApp where
             replace (a:as) | Todo.Domain.id a == Todo.Domain.id todo = todo : as
                            | otherwise                               = replace as
 
-  repoGetById :: UUID -> TestApp (Maybe Todo)
   repoGetById identifier =
     find ((== identifier) . Todo.Domain.id) <$> State.get
 
-  repoDelete :: UUID -> TestApp ()
   repoDelete identifier = do
     State.modify $ filter ((/=identifier) . Todo.Domain.id)
     return ()
 
-instance Repo (ExceptT e TestApp) where
+instance Repo (ExceptT e TestTodoM) where
   repoSelectAll = lift repoSelectAll
   repoInsert = lift . repoInsert
   repoUpdate = lift . repoUpdate
   repoGetById = lift . repoGetById
   repoDelete = lift . repoDelete
 
-runTodoApp :: TestApp a -> StdGen -> [Todo] -> (a, [Todo])
-runTodoApp app =
+testTodo :: TestTodoM a -> StdGen -> [Todo] -> (a, [Todo])
+testTodo app =
   runState . evalRandT (runApp app)
 
-runTodoAppWithSeed :: TestApp a -> Int -> [Todo] -> (a, [Todo])
-runTodoAppWithSeed app seed =
-  runTodoApp app $ mkStdGen seed
+testTodoWithSeed :: TestTodoM a -> Int -> [Todo] -> (a, [Todo])
+testTodoWithSeed app seed =
+  testTodo app $ mkStdGen seed
 
--- test/TodoSpec.hs
+++ test/TodoSpec.hs
@@ -8,7 +8,7 @@ import qualified Hedgehog.Gen        as Gen
 import qualified Hedgehog.Range      as Range
 import           Test.Hspec          (Spec, describe, it, shouldBe)
 import           Test.Hspec.Hedgehog
-import           TestTodoApp
+import           TestTodoApp         (testTodoWithSeed)
 import           Todo.Domain
 
 testUUID :: UUID
@@ -22,33 +22,33 @@ spec = do
       let
         request = CreateTodoRequest "desc"
         expectedTodo = Todo testUUID "desc" False
-      in runTodoAppWithSeed (create request) 0 [] `shouldBe` (expectedTodo, [expectedTodo])
+      in testTodoWithSeed (create request) 0 [] `shouldBe` (expectedTodo, [expectedTodo])
 
   describe "Modify" $ do
     let
       modifiedTodo = Todo testUUID "desc" False
 
     it "should fail on missing todo" $
-      runTodoAppWithSeed (modify testUUID modifiedTodo) 0 [] `shouldBe` (Left ModifyNotExists, [])
+      testTodoWithSeed (modify testUUID modifiedTodo) 0 [] `shouldBe` (Left ModifyNotExists, [])
 
     it "should fail on not matching ids" $
-      runTodoAppWithSeed (modify nil modifiedTodo) 0 [] `shouldBe` (Left ModifyIdentifierMismatch, [])
+      testTodoWithSeed (modify nil modifiedTodo) 0 [] `shouldBe` (Left ModifyIdentifierMismatch, [])
 
     it "should update existing todo" $
       let
         existingTodo = Todo testUUID "other" True
       in
-        runTodoAppWithSeed (modify testUUID modifiedTodo) 0 [existingTodo] `shouldBe` (Right modifiedTodo, [modifiedTodo])
+        testTodoWithSeed (modify testUUID modifiedTodo) 0 [existingTodo] `shouldBe` (Right modifiedTodo, [modifiedTodo])
 
   describe "Delete" $ do
     it "should fail on missing todo" $
-      runTodoAppWithSeed (delete testUUID) 0 [] `shouldBe` (Left DeleteNotExists, [])
+      testTodoWithSeed (delete testUUID) 0 [] `shouldBe` (Left DeleteNotExists, [])
 
     it "should delete existing todo" $
       let
         existingTodo = Todo testUUID "other" True
       in
-        runTodoAppWithSeed (delete testUUID) 0 [existingTodo] `shouldBe` (Right (), [])
+        testTodoWithSeed (delete testUUID) 0 [existingTodo] `shouldBe` (Right (), [])
 
   describe "Patch" $ do
     let
@@ -62,22 +62,22 @@ spec = do
             testUUID
             (fromJust (fmap fromStrict txt <|> Just (description existingTodo)))
             (fromJust (done <|> Just (completed existingTodo)))
-      runTodoAppWithSeed (patch testUUID patchTodo) 0 [existingTodo] === (Right savedTodo, [savedTodo])
+      testTodoWithSeed (patch testUUID patchTodo) 0 [existingTodo] === (Right savedTodo, [savedTodo])
     it "should fail on missing id" $
       let
         patchTodo = TodoM Nothing Nothing Nothing
       in
-        runTodoAppWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left MissingId, [])
+        testTodoWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left MissingId, [])
     it "should fail on not matching ids" $
       let
         patchTodo = TodoM (Just nil) Nothing Nothing
       in
-        runTodoAppWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left PatchIdentifierMismatch, [])
+        testTodoWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left PatchIdentifierMismatch, [])
 
     it "should fail on not existing todo" $
       let
         patchTodo = TodoM (Just testUUID) Nothing Nothing
       in
-        runTodoAppWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left PatchNotExists, [])
+        testTodoWithSeed (patch testUUID patchTodo) 0 [] `shouldBe` (Left PatchNotExists, [])
 
 
