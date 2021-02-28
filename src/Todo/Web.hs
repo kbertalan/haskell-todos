092438705709ea28c3a7b2f4d14701c592845459
Bertalan Kis <kbertalan@gmail.com>
2021-02-28 15:44:42 +0100

    extract function as hlint suggested

--- src/Todo/Web.hs
+++ src/Todo/Web.hs
@@ -7,9 +7,11 @@ where
 
 import App.Error (catch, catchLast)
 import App.Paging (Page (..))
-import App.Web (Scotty, jsonError)
+import App.Web (Action, Scotty, jsonError)
 import Control.Monad (when)
+import Control.Monad.Identity (Identity)
 import Control.Monad.Trans (MonadIO, lift)
+import Data.Aeson (FromJSON)
 import Data.Text.Lazy (unpack)
 import Data.UUID (UUID)
 import Network.HTTP.Types.Status (status201, status400, status404)
@@ -19,6 +21,7 @@ import Todo.Domain
     Logic,
     MissingFields (..),
     NotExists (..),
+    TodoM,
     create,
     delete,
     identifier,
@@ -49,26 +52,27 @@ todoApi = do
     limit <- param "limit" `rescue` const (return 20)
     lift (showPage (Page offset limit)) >>= json
 
-  post "/todo" $ do
-    jsonData >>= lift . create >>= \r -> do
-      status status201
-      json r
+  post "/todo" $
+    jsonData >>= lift . create >>= \r ->
+      status status201 >> json r
 
-  put "/todo/:id" $ do
-    idValue <- Identifier <$> param "id"
-    todo <- jsonData
-    when (idValue /= identifier todo) identifierError
-    lift (modify todo) >>= handleModifyError >>= json
+  put "/todo/:id" $
+    obtainTodo >>= lift . modify >>= handleModifyError >>= json
 
-  W.patch "/todo/:id" $ do
-    idValue <- Identifier <$> param "id"
-    todo <- jsonData
-    when (idValue /= identifier todo) identifierError
-    lift (Todo.Domain.patch todo) >>= handlePatchError >>= json
+  W.patch "/todo/:id" $
+    obtainTodo >>= lift . Todo.Domain.patch >>= handlePatchError >>= json
 
   W.delete "/todo/:id" $
     param "id" >>= lift . Todo.Domain.delete . Identifier >>= handleDeleteError >>= json
   where
+    obtainTodo :: (MonadIO m, FromJSON (TodoM Identity n)) => Action m (TodoM Identity n)
+    obtainTodo = do
+      idValue <- Identifier <$> param "id"
+      todo <- jsonData
+      when (idValue /= identifier todo) identifierError
+      return todo
+
+    identifierError :: (MonadIO m) => Action m a
     identifierError = jsonError status400 "Identifiers in path and body are different"
     notExistsError = jsonError status404 "Todo with provided identifier has not been found"
     missingFieldsError = jsonError status400 "Could not construct final Todo record"
