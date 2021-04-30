9e7772b0f24df4561ba5b4f031fa33270b323e70
Bertalan Kis <kbertalan@gmail.com>
2021-04-30 21:02:20 +0200

    fix traverseHKD type

--- src/Data/HKD.hs
+++ src/Data/HKD.hs
@@ -10,4 +10,4 @@ class FunctorHKD h where
   mapHKD :: (forall a. m a -> n a) -> h m -> h n
 
 class FunctorHKD h => TraversableHKD h where
-  traverseHKD :: Applicative m => (forall a. m a -> m (n a)) -> h m -> m (h n)
+  traverseHKD :: Applicative e => (forall a. f a -> e (g a)) -> h f -> e (h g)
